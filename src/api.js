d516a8ca3c28f4117c33987541de8423a6bdb7bc
Jacobwkidd <kiddj1@students.westerntc.edu>
2023-11-29 23:52:08 -0600

    tenant

--- src/api.js
+++ src/api.js
@@ -27,14 +27,69 @@ export function insertUser(user){
     return ax.post("users/", user).catch((error) => errorHandler("Error Inserting User:" + error));
 }
 
+
+// TENANT METHOD
+export function getAllTenant(){
+    return ax.get("Tenant/").then(resp => (resp.data)).catch(error => errorHandler("Error Getting All Tenant:" + error));
+}
+
+export function getTenantById(id){
+    return ax.get("Tenant/" + id).then(resp => (resp.data)).catch((error) => errorHandler("Error Getting Tenant By Id:" + error));
+}
+
+export function updateTenant(Tenant){
+    return ax.put("Tenant/" + Tenant.id, Tenant).catch((error) => errorHandler("Error Updating Tenant:" + error));
+}
+
+export function insertTenant(Tenant){
+    return ax.post("Tenant/", Tenant).catch((error) => errorHandler("Error Inserting Tenant:" + error));
+}
+
 export function login(email, password) {
-    return ax.get(`users/?email=${email}&password=${password}`).then(resp => {
+    return ax.get(`Tenant/?email=${email}&password=${password}`).then(resp => {
 		if(resp.data.length == 1){
 			return resp.data[0] // we want to get the first, and hopefully only, user from the resp.data array
 		}else{
 			return null
 		}
 	}).catch((error) => errorHandler(error));
+}
+
+
+// LANDLORD METHOD
+export function getAllLandlord(){
+    return ax.get("Landlord/").then(resp => (resp.data)).catch(error => errorHandler("Error Getting All Landlord:" + error));
+}
+
+
+export function getUserById(id){
+    return ax.get("Landlord/" + id).then(resp => (resp.data)).catch((error) => errorHandler("Error Getting Landlord By Id:" + error));
+}
+
+export function updateLandlord(Landlord){
+    return ax.put("Landlord/" + Landlord.id, Landlord).catch((error) => errorHandler("Error Updating Landlord:" + error));
+}
+
+export function insertLandlord(Landlord){
+    return ax.post("Landlord/", Landlord).catch((error) => errorHandler("Error Inserting Landlord:" + error));
+}
+
+
+// PROPERTY METHOD
+export function getAllProperty(){
+    return ax.get("Property/").then(resp => (resp.data)).catch(error => errorHandler("Error Getting All Property:" + error));
+}
+
+export function getUserById(id){
+    return ax.get("Property/" + id).then(resp => (resp.data)).catch((error) => errorHandler("Error Getting Property By Id:" + error));
+}
+
+export function updateProperty(Property){
+    return ax.put("Property/" + Property.id, Property).catch((error) => errorHandler("Error Updating Property:" + error));
+}
+
+export function insertProperty(Property){
+    return ax.post("Property/", Property).catch((error) => errorHandler("Error Inserting Property:" + error));
 }
 
 function errorHandler(msg){
--- src/views/Tenants.vue
+++ src/views/Tenants.vue
@@ -1,5 +1,54 @@
 <template>
     <div class="Tenants">
-        
+        <button @click="$router.push({name: 'AddTenant' })">Add New Tenant</button>
+        <table border="1" v-if="users && roles">
+            <tr>
+                <th></th>
+                <th>Email</th>
+                <th>Active</th>
+                <th></th>
+            </tr>
+            <tr v-for="t in tenant" :key="t.id">
+                <td>{{t.tenant_user_first_name + " " + t.tenant_user_last_name}}</td>
+                <td>{{t.tenant_user_email}}</td>
+                <td>{{getRoleNameById(u.roleId)}}</td>
+                <td>
+                    <button class="btn btn-primary" @click="editTenant(u.id)">Edit</button>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
+
+<script>
+import {getAllTenants, getAllRoles} from "@/api"
+
+export default {
+    data(){
+        return {
+            tenant: [],
+            roles:[]
+        }
+    },
+    mounted(){
+        debugger;
+        getAlltenant().then(tenant => this.tenant = tenant);
+        getAllRoles().then(roles => this.roles = roles);
+    },
+    methods:{
+        getRoleNameById(id){
+          const role = this.roles.find(r => r.id == id);
+          return role?.name;
+          // Note the nullish operator - I noticed that this method gets called even before the roles have been fetched!
+        },
+        editTenant(id){
+            // console.log("TODO: edit Tenant " + id);
+            this.$router.push({name: 'TenantDetails', params: {TenantId:id}}); // pass through the route
+        }
+    }
+}
+</script>
+
+<style scoped>
+  /*Put CSS styles here*/
+</style>
